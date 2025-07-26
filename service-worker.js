5d50dea83885f218b9f3e6206ce92fbf5e2ba036
antoniogrande81 <antoniogrande81@gmail.com>
2025-07-26 21:57:56 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,6 +16,7 @@ const STATIC_CACHE_URLS = [
   '/dirigenti.html',
   '/contatti.html',
   '/area-riservata.html',
++ '/Area Dirigenti.html',
   '/manifest.json',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
